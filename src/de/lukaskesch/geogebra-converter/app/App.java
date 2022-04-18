86088b9db40339ec25b39bbd9c739874d12a1983
Lukas <lukas.kesch@gmail.com>
2022-04-18 22:16:36 +0200

    implements in place folder creation

---
+++ src/de/lukaskesch/geogebra-converter/app/App.java
@@ -0,0 +1,12 @@
+package app;
+
+import java.io.File;
+
+public class App {
+
+    private final File file;
+
+    public App(File file) {
+        this.file = file;
+    }
+}
---
+++ src/de/lukaskesch/geogebra-converter/gui/GUI.java
@@ -0,0 +1,146 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GUI implements ActionListener {
+
+    private static final String TITLE = "Geogebra Converter";
+    private static final String LOAD_BUTTON_TEXT = "Load file";
+
+    private JLabel label = new JLabel("No file selected");
+    private JFrame frame = new JFrame();
+
+    public GUI() {
+
+        // the clickable button
+        JButton button = new JButton(LOAD_BUTTON_TEXT);
+        button.addActionListener(this);
+
+        // the panel with the button and text
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
+
+        // set up the frame and display it
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle(TITLE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    // process the button clicks
+    public void actionPerformed(ActionEvent e) {
+
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setCurrentDirectory(new java.io.File("."));
+        int result = fileChooser.showOpenDialog(frame);
+        if (result != JFileChooser.APPROVE_OPTION) {
+            return;
+        }
+
+        File selectedFile = fileChooser.getSelectedFile();
+        String filePath = selectedFile.getAbsolutePath();
+        String fileName = selectedFile.getName();
+        System.out.println("Selected filepath: " + filePath);
+        System.out.println("Selected filename: " + fileName);
+        label.setText("Opened file: " + filePath);
+
+        String folderPath = removeFileExtension(filePath);
+        System.out.println("Folder path: " + folderPath);
+
+        try {
+            createNewFolder(folderPath);
+            unzip(filePath);
+        } catch (Exception exception) {
+            // TODO: handle exception
+        }
+    }
+
+    // create one Frame
+    public static void main(String[] args) {
+        new GUI();
+    }
+
+    public boolean createNewFolder(String path) {
+        File theDir = new File(path);
+        if (theDir.exists()) {
+            return false;
+        }
+        theDir.mkdirs();
+        return true;
+    }
+
+    public String removeFileExtension(String fileName) {
+        int pos = fileName.lastIndexOf(".");
+        if (pos > 0) {
+            return fileName.substring(0, pos);
+        }
+        return fileName;
+    }
+   
+
+    public static void unzip(String filePath) throws  IOException {
+        String fileZip = filePath;
+        File destDir = new File("src/unzipTest");
+        byte[] buffer = new byte[1024];
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip));
+        ZipEntry zipEntry = zis.getNextEntry();
+        while (zipEntry != null) {
+            File newFile = newFile(destDir, zipEntry);
+            if (zipEntry.isDirectory()) {
+                if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                    throw new IOException("Failed to create directory " + newFile);
+                }
+            } else {
+                // fix for Windows-created archives
+                File parent = newFile.getParentFile();
+                if (!parent.isDirectory() && !parent.mkdirs()) {
+                    throw new IOException("Failed to create directory " + parent);
+                }
+
+                // write file content
+                FileOutputStream fos = new FileOutputStream(newFile);
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+            }
+            zipEntry = zis.getNextEntry();
+        }
+        zis.closeEntry();
+        zis.close();
+    }
+
+    public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+
+        return destFile;
+    }
+}
