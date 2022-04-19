1ae36355a8aee75f642dde07fd8381f1b466cbd1
Lukas <lukas.kesch@gmail.com>
2022-04-19 21:12:54 +0200

    Implements unzipping of ggb file

--- src/de/lukaskesch/geogebra-converter/app/App.java
+++ src/de/lukaskesch/geogebra-converter/app/App.java
@@ -1,8 +1,12 @@
 package app;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class App {
 
@@ -24,9 +28,22 @@ public class App {
         return fileName;
     }
 
-    public void run() {
+    public boolean run() {
         createWorkingFolder();
         moveFileToWorkingFolder();
+        renameFile();
+
+        try {
+            unzipFile();
+            System.out.println("Conversion successful");
+        } catch (IOException e) {
+            System.err.println("Error while unzipping file: " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        } 
+
+
+        return true;
     }
 
     public void createWorkingFolder() {
@@ -42,6 +59,21 @@ public class App {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void renameFile() {
+        File source = new File(workingFolderPath + "/" + fileName);
+        File dest = new File(workingFolderPath + "/" + fileNameWithoutExtension + ".zip");
+        try {
+            copyFile(source, dest);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void unzipFile() throws IOException {
+        String zipFilePath = workingFolderPath + "/" + fileNameWithoutExtension + ".zip";
+        unzip(zipFilePath, workingFolderPath);
     }
 
     public void copyFile(File from, File to) throws IOException {
@@ -63,5 +95,51 @@ public class App {
         }
         theDir.mkdirs();
         return true;
+    }
+
+    public void unzip(String filePath, String destinationPath) throws IOException {
+        String fileZip = filePath;
+        File destDir = new File(destinationPath);
+        byte[] buffer = new byte[1024];
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip));
+        ZipEntry zipEntry = zis.getNextEntry();
+        while (zipEntry != null) {
+            File newFile = newUnzipedFile(destDir, zipEntry);
+            if (zipEntry.isDirectory()) {
+                if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                    throw new IOException("Failed to create directory " + newFile);
+                }
+            } else {
+                // fix for Windows-created archives
+                File parent = newFile.getParentFile();
+                if (!parent.isDirectory() && !parent.mkdirs()) {
+                    throw new IOException("Failed to create directory " + parent);
+                }
+
+                // write file content
+                FileOutputStream fos = new FileOutputStream(newFile);
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+            }
+            zipEntry = zis.getNextEntry();
+        }
+        zis.closeEntry();
+        zis.close();
+    }
+
+    public File newUnzipedFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+
+        return destFile;
     }
 }
--- src/de/lukaskesch/geogebra-converter/gui/GUI.java
+++ src/de/lukaskesch/geogebra-converter/gui/GUI.java
@@ -17,13 +17,14 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.filechooser.FileFilter;
 
 import app.App;
 
 public class GUI implements ActionListener {
 
     private static final String TITLE = "Geogebra Converter";
-    private static final String LOAD_BUTTON_TEXT = "Load file";
+    private static final String LOAD_BUTTON_TEXT = "Load and convert file";
 
     private JLabel label = new JLabel("No file selected");
     private JFrame frame = new JFrame();
@@ -40,6 +41,7 @@ public class GUI implements ActionListener {
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
+        panel.setSize(200, 100);
         panel.add(button);
         panel.add(label);
 
@@ -56,6 +58,19 @@ public class GUI implements ActionListener {
 
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setCurrentDirectory(new java.io.File("."));
+        fileChooser.setDialogTitle("Select Geogebra file");
+        fileChooser.setFileFilter(new FileFilter() {
+            @Override
+            public boolean accept(File f) {
+                return f.getName().toLowerCase().endsWith(".ggb") || f.isDirectory();
+            }
+
+            @Override
+            public String getDescription() {
+                return "Geogebra files (*.ggb)";
+            }
+        });
+        
         int result = fileChooser.showOpenDialog(frame);
         if (result != JFileChooser.APPROVE_OPTION) {
             return;
@@ -85,52 +100,4 @@ public class GUI implements ActionListener {
 
     
 
-   
-   
-
-    public static void unzip(String filePath) throws  IOException {
-        String fileZip = filePath;
-        File destDir = new File("src/unzipTest");
-        byte[] buffer = new byte[1024];
-        ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip));
-        ZipEntry zipEntry = zis.getNextEntry();
-        while (zipEntry != null) {
-            File newFile = newFile(destDir, zipEntry);
-            if (zipEntry.isDirectory()) {
-                if (!newFile.isDirectory() && !newFile.mkdirs()) {
-                    throw new IOException("Failed to create directory " + newFile);
-                }
-            } else {
-                // fix for Windows-created archives
-                File parent = newFile.getParentFile();
-                if (!parent.isDirectory() && !parent.mkdirs()) {
-                    throw new IOException("Failed to create directory " + parent);
-                }
-
-                // write file content
-                FileOutputStream fos = new FileOutputStream(newFile);
-                int len;
-                while ((len = zis.read(buffer)) > 0) {
-                    fos.write(buffer, 0, len);
-                }
-                fos.close();
-            }
-            zipEntry = zis.getNextEntry();
-        }
-        zis.closeEntry();
-        zis.close();
-    }
-
-    public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
-        File destFile = new File(destinationDir, zipEntry.getName());
-
-        String destDirPath = destinationDir.getCanonicalPath();
-        String destFilePath = destFile.getCanonicalPath();
-
-        if (!destFilePath.startsWith(destDirPath + File.separator)) {
-            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
-        }
-
-        return destFile;
-    }
 }
