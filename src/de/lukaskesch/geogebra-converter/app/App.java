affb5fb29611f886c0b239471d0b577ade422cb1
Lukas <lukas.kesch@gmail.com>
2022-04-20 10:34:42 +0200

    refactoring xml parser

--- src/de/lukaskesch/geogebra-converter/app/App.java
+++ src/de/lukaskesch/geogebra-converter/app/App.java
@@ -1,23 +1,17 @@
 package app;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
+
+import app.files.XMLParser;
+
+
+
 
 public class App {
 
@@ -46,7 +40,7 @@ public class App {
 
         try {
             unzipFile();
-            Document  document = getParsedXML(workingFolderPath + "/geogebra.xml");
+            Document  document = XMLParser.getParsedXML(workingFolderPath + "/geogebra.xml");
             System.out.println("Conversion successful");
         } catch (IOException e) {
             System.err.println("Error while unzipping file: " + e.getMessage());
@@ -111,74 +105,8 @@ public class App {
 
     
 
-    public Document getParsedXML(String filePath)
-    {
-        // Instantiate the Factory
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-
-        Document document = null;
-
-
-        try (InputStream is = new FileInputStream(new File(filePath))) {
-
-            // parse XML file
-            DocumentBuilder db = dbf.newDocumentBuilder();
-
-            // read from a project's resources folder
-            document = db.parse(is);
-
-            System.out.println("Root Element :" + document.getDocumentElement().getNodeName());
-            System.out.println("------");
-
-            if (document.hasChildNodes()) {
-                printNote(document.getChildNodes());
-            }
-
-        } catch (ParserConfigurationException | SAXException | IOException e) {
-            e.printStackTrace();
-        }
-
-        return document;
-    }
-
-    private static void printNote(NodeList nodeList) {
-
-        for (int count = 0; count < nodeList.getLength(); count++) {
-
-            Node tempNode = nodeList.item(count);
-
-            // make sure it's element node.
-            if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
-
-                // get node name and value
-                System.out.println("\nNode Name =" + tempNode.getNodeName() + " [OPEN]");
-                System.out.println("Node Value =" + tempNode.getTextContent());
-
-                if (tempNode.hasAttributes()) {
-
-                    // get attributes names and values
-                    NamedNodeMap nodeMap = tempNode.getAttributes();
-                    for (int i = 0; i < nodeMap.getLength(); i++) {
-                        Node node = nodeMap.item(i);
-                        System.out.println("attr name : " + node.getNodeName());
-                        System.out.println("attr value : " + node.getNodeValue());
-                        
-                    }
-
-                }
-
-                if (tempNode.hasChildNodes()) {
-                    // loop again if has child nodes
-                    printNote(tempNode.getChildNodes());
-                }
-
-                System.out.println("Node Name =" + tempNode.getNodeName() + " [CLOSE]");
-
-            }
-
-        }
-
-    }
+    
+    
 
     
 
--- src/de/lukaskesch/geogebra-converter/app/files/XMLParser.java
+++ src/de/lukaskesch/geogebra-converter/app/files/XMLParser.java
@@ -1,5 +1,88 @@
 package app.files;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class XMLParser {
+
+    public static Document getParsedXML(String filePath) {
+        // Instantiate the Factory
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+        Document document = null;
+
+        try (InputStream is = new FileInputStream(new File(filePath))) {
+
+            // parse XML file
+            DocumentBuilder db = dbf.newDocumentBuilder();
+
+            // read from a project's resources folder
+            document = db.parse(is);
+
+            System.out.println("Root Element :" + document.getDocumentElement().getNodeName());
+            System.out.println("------");
+
+            if (document.hasChildNodes()) {
+                printNote(document.getChildNodes());
+            }
+
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+
+        return document;
+    }
+
     
+
+    private static void printNote(NodeList nodeList) {
+
+        for (int count = 0; count < nodeList.getLength(); count++) {
+
+            Node tempNode = nodeList.item(count);
+
+            // make sure it's element node.
+            if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                // get node name and value
+                System.out.println("\nNode Name =" + tempNode.getNodeName() + " [OPEN]");
+                System.out.println("Node Value =" + tempNode.getTextContent());
+
+                if (tempNode.hasAttributes()) {
+
+                    // get attributes names and values
+                    NamedNodeMap nodeMap = tempNode.getAttributes();
+                    for (int i = 0; i < nodeMap.getLength(); i++) {
+                        Node node = nodeMap.item(i);
+                        System.out.println("attr name : " + node.getNodeName());
+                        System.out.println("attr value : " + node.getNodeValue());
+
+                    }
+
+                }
+
+                if (tempNode.hasChildNodes()) {
+                    // loop again if has child nodes
+                    printNote(tempNode.getChildNodes());
+                }
+
+                System.out.println("Node Name =" + tempNode.getNodeName() + " [CLOSE]");
+
+            }
+
+        }
+
+    }
 }
